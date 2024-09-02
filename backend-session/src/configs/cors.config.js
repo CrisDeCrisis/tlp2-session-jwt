f658e1f924a5e1cbb34a271a1f9e8573221fdbf1
CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-09-02 16:18:35 -0300

    delete backend-session

--- backend-session/src/configs/cors.config.js
+++
@@ -1,8 +0,0 @@
-export const corsOptions = { // Permitir solicitudes desde el front-end
-    origin: [
-        'http://localhost:5500',
-        'http://localhost:3000',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // Habilitar envío de cookies
-}
